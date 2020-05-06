82a2e0c9db32e38434f2da568f9bc5193e280fd2
davy1ex <dezy1ex@gmail.com>
2020-05-06 23:44:23 +0400

    active on click

---
+++ js/style.js
@@ -0,0 +1,4 @@
+var path_parse = String(window.location).split('/');
+var id_element = String(path_parse[path_parse.length-1]).split('.html')[0];
+console.log(typeof(id_element));
+document.getElementById(id_element).className += " active";
