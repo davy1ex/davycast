a4ffb2240f29804722a76814af3bd7c0f7bbfaf7
dezy1ex <dezy1ex@gmail.com>
2020-05-31 20:47:52 +0400

    added speakers and animations

--- js/style.js
+++ js/style.js
@@ -1,4 +1,3 @@
 var path_parse = String(window.location).split('/');
 var id_element = String(path_parse[path_parse.length-1]).split('.html')[0];
-console.log(typeof(id_element));
 document.getElementById(id_element).className += " active";
