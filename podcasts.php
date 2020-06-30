277dfcdacb3d59a0422070d73ff8790a1f0bcd64
davy1ex <dezy1ex@gmail.com>
2020-06-30 08:00:03 +0400

    adding sidebar & podcasts page

---
+++ podcasts.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template Name: Podcasts
+*/
+?>
+
+<?php wp_head()?> 
+<?php get_header(); ?>
+
+
+<div class="content-wrapper">
+    <div class="first column">
+        <div class="cell-podcasts">
+            <div class="podcast">
+                <img src="img/podcast1.png" alt="">
+            </div>
+
+            <div class="podcast">
+                <img src="img/podcast2.png" alt="">
+            </div>
+
+            <div class="podcast">
+                <img src="img/podcast3.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="second column side-bar">
+        <div class="widget instagrams">
+            tut instagram
+        </div>
+
+        <div class="widget twitter">
+            tut twitter
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
